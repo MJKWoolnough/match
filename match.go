8a24bbbe7a166c708700784b9418b52feb8c6bcb
Michael Woolnough <michael.woolnough@gmail.com>
2025-07-06 18:10:54 +0100

    added methods to do a partial match which can be continued later

--- match.go
+++ match.go
@@ -51,17 +51,42 @@ func (s *StateMachine[T]) AddString(str string, value T) error {
 }
 
 func (s *StateMachine[T]) Match(str string) T {
-	state := uint32(1)
+	return s.states[s.matchState(str, 1)].value
+}
 
+func (s *StateMachine[T]) matchState(str string, state uint32) uint32 {
 	for _, c := range strToBytes(str) {
 		state = s.states[state].states[c]
 	}
 
-	return s.states[state].value
+	return state
 }
 
 func strToBytes(str string) []byte {
 	return unsafe.Slice(unsafe.StringData(str), len(str))
+}
+
+type State[T comparable] struct {
+	sm    *StateMachine[T]
+	state uint32
+}
+
+func (s *StateMachine[T]) MatchState(str string) State[T] {
+	return State[T]{
+		sm:    s,
+		state: s.matchState(str, 1),
+	}
+}
+
+func (s *State[T]) Match(str string) T {
+	return s.sm.states[s.sm.matchState(str, s.state)].value
+}
+
+func (s *State[T]) MatchState(str string) State[T] {
+	return State[T]{
+		sm:    s.sm,
+		state: s.sm.matchState(str, s.state),
+	}
 }
 
 var ErrAmbiguous = errors.New("ambiguous states")
