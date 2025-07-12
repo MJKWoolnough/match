0e18bfc08eda81d7e9977b585ac8b5d2122c651c
Michael Woolnough <michael.woolnough@gmail.com>
2025-07-12 18:55:27 +0100

    added tokeniser for a string with simple wildcards

--- tokeniser.go
+++ tokeniser.go
@@ -1,11 +1,17 @@
 package match
 
-import "vimagination.zapto.org/parser"
+import (
+	"errors"
+
+	"vimagination.zapto.org/parser"
+)
 
 const (
 	tokenStart parser.TokenType = iota
-	tokenChar
 	tokenEnd
+	tokenChar
+	tokenAnyChar
+	tokenRepeat
 )
 
 func simpleStart(t *parser.Tokeniser) (parser.Token, parser.TokenFunc) {
@@ -19,3 +25,35 @@ func simpleString(t *parser.Tokeniser) (parser.Token, parser.TokenFunc) {
 
 	return t.Return(tokenChar, simpleString)
 }
+
+func partialStringStart(t *parser.Tokeniser) (parser.Token, parser.TokenFunc) {
+	return t.Return(tokenStart, partialString)
+}
+
+func partialString(t *parser.Tokeniser) (parser.Token, parser.TokenFunc) {
+	switch t.Peek() {
+	case -1:
+		return t.Return(tokenEnd, (*parser.Tokeniser).Done)
+	case '*':
+		return t.Return(tokenAnyChar, partialStringWildcard)
+	case '\\':
+		t.Next()
+		t.Get()
+
+		if t.Peek() == -1 {
+			return t.ReturnError(ErrInvalidEscape)
+		}
+	}
+
+	t.Next()
+
+	return t.Return(tokenChar, partialString)
+}
+
+func partialStringWildcard(t *parser.Tokeniser) (parser.Token, parser.TokenFunc) {
+	t.Next()
+
+	return t.Return(tokenRepeat, partialString)
+}
+
+var ErrInvalidEscape = errors.New("invalid escape sequence")
